eb98dc14faf1f4eebe17478298a3ab5f02498f32
kioanhfortin <kioanhfortin@gmail.com>
2023-05-08 11:14:04 -0400

    add on github

---
+++ get_next_line_bonus.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfortin <kfortin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/02 16:31:46 by kfortin           #+#    #+#             */
+/*   Updated: 2022/07/07 11:50:02 by kfortin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# ifndef FD_SIZE
+#  define FD_SIZE 65535
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *s);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memset(void *b, int c, size_t len);
+char	*ft_strdup(char *src, int end, int flag);
+char	*ft_strdup_n(char *src, int end);
+int		ft_strchr(char *stash);
+int		ft_last_line(char **stash, char ***res, int size_read);
+char	*ft_stock_line(char **stash, char ***res, int end);
+#endif
